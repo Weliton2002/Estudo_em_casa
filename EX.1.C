368f6a5b377afc25fa4f289c391fc1abb8c22ab5
Weliton Alves <150181328+Weliton2002@users.noreply.github.com>
2023-12-24 17:27:53 -0400

    Resolução dos exercícios 1 e 2.

---
+++ EX.1.C
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // Exibição da mensagem "Olá Mundo!"
+    printf("Ola Mundo!\n");
+
+    return 0;
+}
---
+++ EX.2.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*Faça um programa que declare uma variável inteira e mostre seu conteúdo para o
+usuário. Execute o programa algumas vezes. Qual o significado do valor que está sendo
+impresso pelo programa?*/
+
+int main() {
+    // Declaração da variável inteira
+    int x;
+
+    // Atribuição de um valor à variável
+    x = 42;
+
+    // Exibição do conteúdo da variável para o usuário
+    printf("O conteudo da variavel e: %d\n", x);
+
+    return 0;
+}
